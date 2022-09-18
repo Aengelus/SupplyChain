e32ce4f933a0a30952f32fb0afc735bcfd617056
Sascha Getzinger <getzinger.sascha@gmail.com>
2022-09-18 15:16:37 +0200

    Project finished

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,23 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const apiKey = fs.readFileSync(".apiKey").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/"+apiKey)
+      },
+      network_id: 4,
+      gas: 5500000,
+      gasprice: 20000000000,
     }
   },
   compilers: {
